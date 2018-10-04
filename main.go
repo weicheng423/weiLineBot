3d33a0d3b1f9e0b06192d6b64c9429df3e42859c
wei <weicheng423@gmail.com>
2018-10-04 17:54:18 +0800

    update for wei

--- main.go
+++ main.go
@@ -47,12 +47,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-		}
+			linebot.NewTextMessage(message.ID+"aaaaa:"+message.Text+" OK!")).Do();
+			// switch message := event.Message.(type) {
+			// case *linebot.TextMessage:
+			// 	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+			// 		log.Print(err)
+			// 	}
+			// }
+		// }
 	}
 }
